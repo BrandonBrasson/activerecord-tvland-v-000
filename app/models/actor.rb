4d198cfefab949ec378e5d3c55145323a646fa01
Brandon Brasson <rocksong622@gmail.com>
2019-10-30 20:05:14 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,14 +1,7 @@
 class Actor < ActiveRecord::Base
-  has_many :characters
-   has_many :shows, through: :characters
 
-   def full_name
-     "#{self.first_name} #{self.last_name}"
+  def change
+   create_table :actor do |a|
+     a.string :name
+  
    end
-
-   def list_roles
-     characters.collect do |character|
-       "#{character.name} - #{character.show.name}"
-     end
-   end
- end
